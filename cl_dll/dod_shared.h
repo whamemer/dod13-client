ea4d8d0394d7e6ab43499b4956ba669a410b4133
whamemer <vladislav.vlad.stepanenko@mail.ru>
2022-01-25 15:45:49 +0300

    Create dod_shared header

---
+++ cl_dll/dod_shared.h
@@ -0,0 +1,139 @@
+enum VOICECOMSLOTS_e
+{
+
+};
+
+enum KNIFE_e
+{
+
+};
+
+enum GREASEGUN_e
+{
+
+};
+
+enum CAL30_e
+{
+
+};
+
+enum BAR_e
+{
+
+};
+
+enum BREN_e
+{
+
+};
+
+enum COLT_e
+{
+
+};
+
+enum ENFIELD_e
+{
+
+};
+
+enum SCOPEDENFIELD_e
+{
+
+};
+
+enum FG42_e
+{
+
+};
+
+enum GARAND_e
+{
+
+};
+
+enum K43_e
+{
+
+};
+
+enum KAR_e
+{
+
+};
+
+enum LUGER_e
+{
+
+};
+
+enum M1CARBINE_e
+{
+
+};
+
+enum MG34_e
+{
+
+};
+
+enum MG42_e
+{
+
+};
+
+enum MP40_e
+{
+
+};
+
+enum MP44_e
+{
+
+};
+
+enum SCOPEDKAR_e
+{
+
+};
+
+enum SPRING_e
+{
+
+};
+
+enum STEN_e
+{
+
+};
+
+enum THOMPSON_e
+{
+
+};
+
+enum WEBLEY_e
+{
+
+};
+
+enum BAZOOKA_e
+{
+
+};
+
+enum PIAT_e
+{
+
+};
+
+enum PSCHRECK_e
+{
+
+};
+
+enum Mortar_e
+{
+
+};
