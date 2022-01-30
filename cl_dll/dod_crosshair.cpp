51bde5a4a6b836249db0e96a73b0f0f6a6ef608a
whamemer <vladislav.vlad.stepanenko@mail.ru>
2022-01-30 18:50:51 +0300

    Create new client files

---
+++ cl_dll/dod_crosshair.cpp
@@ -0,0 +1,60 @@
+
+CHudDoDCrossHair::Init( void )
+{
+
+}
+
+int CHudDoDCrossHair::VidInit( void )
+{
+
+}
+
+void CHudDoDCrossHair::Reset( void )
+{
+
+}
+
+int CHudDoDCrossHair::MsgFunc_ClanTimer( const char *pszName, int iSize, void *pbuf )
+{
+
+}
+
+int CHudDoDCrossHair::Draw( float fltime )
+{
+
+}
+
+void CHudDoDCrossHair::DrawDynamicCrossHair( void )
+{
+
+}
+
+void CHudDoDCrossHair::DrawCustomCrossHair( int style )
+{
+
+}
+
+int CHudDoDCrossHair::GetCrossHairWidth( void )
+{
+
+}
+
+float CHudDoDCrossHair::GetCurrentWeaponAccuracy( void )
+{
+
+}
+
+bool CHudDoDCrossHair::ShouldDrawCrossHair( void )
+{
+
+}
+
+void CHudDoDCrossHair::DrawSpectatorCrossHair( void )
+{
+
+}
+
+void CHudDoDCrossHair::DrawClanTimer( float fltime )
+{
+
+}
