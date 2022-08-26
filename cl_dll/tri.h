7ae93ca8e8a39c572f9b22e85d98455ee286f1a8
whamemer <vladislav.vlad.stepanenko@mail.ru>
2022-08-26 23:51:11 +0300

    Update CBaseDoDParticle

--- cl_dll/tri.h
+++ cl_dll/tri.h
@@ -1,22 +1,27 @@
 #include "../particleman/particleman/src/public/particleman.h"
+#include "vector.h"
 
 class CBaseDoDParticle : public CBaseParticle
 {
 public:
-    void Think( float time );
-    void Draw( void );
-    void Animate( float time );
-    void AnimateAndDie( float time );
-    void Expand( float time );
-    void Contract( float time );
-    void Fade( float time );
-    void Spin( float time );
-    void CalculateVelocity( float time );
-    void CheckCollision( float time );
-    void Touch( Vector *p_pos, Vector *p_normal, int index );
-    void Die( void );
-    void Force( void );
-    void InitializeSprite( Vector *p_org, Vector *p_normal, model_s *sprite, float size, float brightness );
+    virtual void Think( float time ) { g_pParticleMan->CoreThink( this, time ); }
+    virtual void Draw( void ) { g_pParticleMan->CoreDraw( this ); }
+    virtual void Animate( float time ) { g_pParticleMan->CoreAnimate( this, time ); }
+    virtual void AnimateAndDie( float time ) { g_pParticleMan->CoreAnimateAndDie( this, time ); }
+    virtual void Expand( float time ) { g_pParticleMan->CoreExpand( this, time ); }
+    virtual void Contract( float time ) { g_pParticleMan->CoreContract( this, time ); }
+    virtual void Fade( float time ) { g_pParticleMan->CoreFade( this, time ); }
+    virtual void Spin( float time ) { g_pParticleMan->CoreSpin( this, time ); }
+    virtual void CalculateVelocity( float time ) { g_pParticleMan->CoreCalculateVelocity( this, time ); }
+    virtual void CheckCollision( float time ) { g_pParticleMan->CoreCheckCollision( this, time ); }
+    virtual void Touch( Vector pos, Vector normal, int index ) { g_pParticleMan->CoreTouch( this, pos, normal, index ); }
+    virtual void Die( void ) { g_pParticleMan->CoreDie( this ); }
+    virtual void Force( void ) { g_pParticleMan->CoreForce( this ); }
+
+    virtual void InitializeSprite( Vector org, Vector normal, model_s *sprite, float size, float brightness )
+    {
+	    g_pParticleMan->CoreInitializeSprite( this, org, normal, sprite, size, brightness );
+	}
 };
 
 class CDoDParticle : public CBaseDoDParticle
@@ -27,7 +32,7 @@ public:
     void Think( float time );
     void Force( void );
     void Die( void );
-    void Touch( Vector *p_pos, Vector *p_normal, int index );
+    void Touch( Vector pos, Vector normal, int index );
 
 private:
     bool m_bInsideSmoke;
@@ -54,7 +59,7 @@ class CDoDRocketTrail : public CBaseDoDParticle
 {
 public:
     void Think( float time );
-    CDoDRocketTrail Create( Vector *p_org, Vector *p_normal, model_s *sprite, float size, float brightness, const char *classname );
+    CDoDRocketTrail Create( Vector org, Vector normal, model_s *sprite, float size, float brightness, const char *classname );
 
 private:
     bool m_bRocketTrail;
@@ -64,7 +69,7 @@ class CDoDDirtExploDust : public CBaseDoDParticle
 {
 public:
     void Think( float time );
-    CDoDDirtExploDust Create( Vector *p_org, Vector *p_normal, model_s *sprite, float size, float brightness, const char *classname );
+    CDoDDirtExploDust Create( Vector org, Vector normal, model_s *sprite, float size, float brightness, const char *classname );
 
 private:
     bool m_bFire;
@@ -75,8 +80,8 @@ class CDoDSnowFlake : public CDoDParticle
 {
 public:
     void Think( float time );
-    void Touch( Vector *p_pos, Vector *p_normal, int index );
-    CDoDSnowFlake Create( Vector *p_org, Vector *p_normal, model_s *sprite, float size, float brightness, const char *classname );
+    void Touch( Vector pos, Vector normal, int index );
+    CDoDSnowFlake Create( Vector org, Vector normal, model_s *sprite, float size, float brightness, const char *classname );
 
 private:
     bool m_bSpiral;
