b4413cafc4b22e111793a26dbb18e16629af4a20
whamemer <vladislav.vlad.stepanenko@mail.ru>
2022-08-31 16:58:25 +0300

    events: create functions in ev_common.cpp

---
+++ cl_dll/ev_hldm.cpp
@@ -0,0 +1,22 @@
+#include "hud.h"
+#include "cl_util.h"
+#include "const.h"
+#include "entity_state.h"
+#include "cl_entity.h"
+#include "entity_types.h"
+#include "usercmd.h"
+#include "pm_defs.h"
+#include "pm_materials.h"
+
+#include "eventscripts.h"
+#include "ev_hldm.h"
+
+#include "r_efx.h"
+#include "event_api.h"
+#include "event_args.h"
+#include "in_defs.h"
+
+#include <string.h>
+
+#include "r_studioint.h"
+#include "com_model.h"
---
+++ cl_dll/ev_hldm.h
@@ -0,0 +1,19 @@
+//========= Copyright (c) 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================
+
+#pragma once
+#if !defined ( EV_HLDMH )
+#define EV_HLDMH
+
+// bullet types
+typedef	enum
+{
+	BULLET_NONE = 0,
+}Bullet;
+
+
+#endif // EV_HLDMH
