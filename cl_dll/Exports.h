4e2fcf605050bd9cda2b8edb07db02fa4535a09a
whamemer <vladislav.vlad.stepanenko@mail.ru>
2022-09-01 15:48:53 +0300

    add some sdk files

---
+++ cl_dll/Exports.h
@@ -0,0 +1,15 @@
+//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================
+#pragma once
+#if !defined( EXPORTS )
+#define EXPORTS
+
+#include "APIProxy.h"
+
+extern cldll_func_dst_t *g_pcldstAddrs;
+
+#endif // !EXPORTS
