4455b91bc6332a51f3fda0a48cd8619238efcf58
whamemer <vladislav.vlad.stepanenko@mail.ru>
2022-09-04 13:49:41 +0300

    update dod_gamerules.cpp ( client part )

---
+++ dlls/dod_gamerules.h
@@ -0,0 +1,13 @@
+//========= Copyright (c) 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================
+#pragma once
+#if !defined( GAMERULES_H )
+#define GAMERULES_H
+
+const char *g_szTeamNames[4][128];
+
+#endif // !GAMERULES_H
